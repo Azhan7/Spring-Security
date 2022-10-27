d53fc0b264cb341477e67b89846a84a8351b539c
Azhan7 <azhanali777@gmail.com>
2022-10-28 02:52:06 +0500

    add logs

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,12 +1,15 @@
 package com.example.demo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class DemoApplication {
 
     public static void main(String[] args) {
+        log.info("Application Started");
         SpringApplication.run(DemoApplication.class, args);
     }
 
